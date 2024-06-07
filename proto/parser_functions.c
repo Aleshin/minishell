97d8cd585465fc1becd71344f85f665246640bca
Sergey Aleshin <collectorofmail@gmail.com>
2024-06-07 21:10:28 +0200

    pipes problem

--- proto/parser_functions.c
+++ proto/parser_functions.c
@@ -18,13 +18,13 @@ t_ast_node	*rule_command_line(t_Token_node **token, t_ast_node *ast_node)
 	reverse_node = rule_command(token); // Command line -> Command
 	if (reverse_node != NULL)
 	{
-		add_child_node(ast_node, reverse_node);
+		add_child_node(ast_node, reverse_node); // Add command node with exec node with or no args
 		if ((*token)->next_token != NULL
 			&& (*token)->next_token->value[0] == '|')
 		{
-			*token = (*token)->next_token;
 			reverse_node = create_ast_node(PIPE, (*token)->value);
-			add_child_node(ast_node, reverse_node);
+			add_child_node(ast_node, reverse_node); // add pipe token
+			*token = (*token)->next_token;
 			if ((*token)->next_token != NULL)
 			{
 				*token = (*token)->next_token;
@@ -37,23 +37,29 @@ t_ast_node	*rule_command_line(t_Token_node **token, t_ast_node *ast_node)
 
 t_ast_node	*rule_command(t_Token_node **token)
 {
-	t_ast_node	*command_node;
-	t_ast_node	*reverse_node;
+	t_ast_node		*command_node;
+	t_ast_node		*reverse_node;
+	t_Token_node	**temp_token;
+
+(void) temp_token;
 
 	command_node = NULL;
 	reverse_node = rule_executable(token); // Command -> Executable
 	if (reverse_node != NULL)
 	{
-		command_node = create_ast_node(command, "");
+		command_node = create_ast_node(command, ""); // Create command node and add exec node inside
 		add_child_node(command_node, reverse_node);
 	}
 	if ((*token)->next_token != NULL)
 	{
-		reverse_node = rule_arguments(&(*token)->next_token); // Command -> Arguments
+//		temp_token = token;
+//		*temp_token = (*token)->next_token;
+		*token = (*token)->next_token;
+		reverse_node = rule_arguments(token); // Command -> Arguments
 		if (reverse_node != NULL)
 		{
-			*token = (*token)->next_token;
-			add_child_node(command_node, reverse_node);
+//			*token = (*token)->next_token;
+			add_child_node(command_node, reverse_node); // Add arguments node with list of arguments inside to command node
 		}
 	}
 	return (command_node);
@@ -66,7 +72,7 @@ t_ast_node	*rule_executable(t_Token_node **token) // bottom of tree
 	ast_node = NULL;
 	if ((*token)->type == lexem)
 	{
-		ast_node = create_ast_node(executable, (*token)->value);
+		ast_node = create_ast_node(executable, (*token)->value); // return exec node to command function
 	}
 	return (ast_node);
 }
@@ -80,9 +86,11 @@ t_ast_node	*rule_arguments(t_Token_node **token)
 	reverse_node = rule_argument(token); // Arguments -> Argument (first)
 	if (reverse_node != NULL)
 	{
-		arguments_node = create_ast_node(arguments, "");
+		arguments_node = create_ast_node(arguments, ""); // Create args node and add 1-st arg node inside 
 		add_child_node(arguments_node, reverse_node);
 		arguments_node = recursive_arguments(token, arguments_node); // Arguments -> Argument (all next)
+		if ((*token)->next_token != NULL)
+			*token = (*token)->next_token;
 	}
 	return (arguments_node);
 }
@@ -91,16 +99,17 @@ t_ast_node	*recursive_arguments(t_Token_node **token, t_ast_node *ast_node)
 {
 	t_ast_node	*reverse_node;
 
-	if ((*token)->next_token != NULL && (*token)->next_token->type == lexem)
-	{
-		*token = (*token)->next_token;
+//	if ((*token)->next_token != NULL && (*token)->next_token->type == lexem)
+//	{
 		reverse_node = rule_argument(token); // -> argument
 		if (reverse_node != NULL)
 		{
 			add_child_node(ast_node, reverse_node);
+			if ((*token)->next_token == NULL)
+				return (NULL);
 			ast_node = recursive_arguments(token, ast_node); // Recursive argument -> argument
 		}
-	}
+//	}
 	return (ast_node);
 }
 
@@ -112,6 +121,8 @@ t_ast_node	*rule_argument(t_Token_node **token) // bottom of tree
 	if ((*token)->type == lexem)
 	{
 		ast_node = create_ast_node(argument, (*token)->value);
+		if ((*token)->next_token != NULL)
+			*token = (*token)->next_token;
 	}
 	return (ast_node);
 }
