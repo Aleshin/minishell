ce08922a83dab2fc3eb28461329a5aece0b12017
Sergey Aleshin <saleshin@car12s2.42barcelona.com>
2024-09-10 01:48:17 +0200

    "exit" works in general

--- executor/ft_exit.c
+++ executor/ft_exit.c
@@ -11,9 +11,67 @@
 /* ************************************************************************** */
 #include "minishell.h"
 
-int	ft_exit(t_env **env_list, t_ast_node *command)
+//returns 0 if number is 0, negative or not number
+int	exit_code(char *str)
 {
-    (void) env_list;
-    (void) command;
-    exit (0);
+	long long	res;
+	int			sign;
+	int			is_num;
+
+	res = 0;
+	sign = 1;
+	is_num = -1;
+	while (*str == ' ' || *str == '\n' || *str == '\t')
+		str++;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		is_num = 1;
+		if ((sign == 1 && res > LLONG_MAX / 10)
+			|| (sign == -1 && res * sign < LLONG_MIN / 10))
+			return (-1);
+		res = res * 10 + (*str - '0');
+		str++;
+	}
+	if (is_num == 1 && *str == '\0')
+	{
+		if (sign < 0)
+			res = 256 - (res % 256);
+		else
+			res = res % 256;
+		return (res);
+	}
+	return (-1);
+}
+
+/*
+args (num of args): command->first_child->next_sibling->next_sibling->param
+arg 1: command->first_child->next_sibling->next_sibling->first_child
+*/
+int	ft_exit(t_ast_node *command)
+{
+	int		num;
+	char	*value;
+
+	num = command->first_child->next_sibling->next_sibling->param;
+	if (num > 0)
+	{
+		value = command->first_child->next_sibling
+			->next_sibling->first_child->value;
+		if (exit_code(value) < 0)
+		{
+			printf("exit\nexit: %s: numeric argument required\n", value);
+			exit (2);
+		}
+		if (num == 1)
+			exit (exit_code(value));
+		ft_putstr_fd("exit\nexit: too many arguments\n", STDERR_FILENO);
+		return (130);
+	}
+	exit (0);
 }
--- minishell.h
+++ minishell.h
@@ -29,6 +29,7 @@ ast_tree
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+#include <limits.h>
 # include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
@@ -207,7 +208,7 @@ int				ft_echo(t_ast_node *command);
 t_env			*ft_lstnew_env(char *name, char *value);
 int				ft_unset(t_env **list, t_ast_node *command);
 int				ft_export_node(t_env **lst, char *cur_arg);
-int				ft_exit(t_env **env_list, t_ast_node *command);
+int				ft_exit(t_ast_node *command);
 
 // exec_helpers
 void			free_arr(char **arr);
