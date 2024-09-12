0beab9596a83cd095a7b31207ec6a33e35a502c7
Ekaterina Mikaylova <ekaterinamikaylova@Ekaterinas-MacBook-Air.local>
2024-09-12 16:23:40 +0200

    norm pipes, to check ctrl C

--- executor/pipes.c
+++ executor/pipes.c
@@ -45,11 +45,11 @@ void	ft_child_process(t_pipe *pipes, t_ast_node *command,
 	// Execute the command (external or builtin)
 	if (command->first_child && command->first_child->next_sibling)
 	{
-        if (is_builtin(command))
-            status = ft_exec_builtin(command, env_list);
-        else
-            status = ft_exec_command(command, env_list);
-    }
+		if (is_builtin(command))
+			status = ft_exec_builtin(command, env_list);
+		else
+			status = ft_exec_command(command, env_list);
+	}
 	// Handle exit status and terminate child process
 	if (status == 13 || status == 8)
 		status = 126;
@@ -58,20 +58,18 @@ void	ft_child_process(t_pipe *pipes, t_ast_node *command,
 	exit(status);
 }
 
-void ft_parent_process(t_pipe *pipes, t_ast_node *command)
+void	ft_parent_process(t_pipe *pipes, t_ast_node *command)
 {
     // Close the old input fd if it's not stdin
-    if (pipes->fd_in != 0)
-        close(pipes->fd_in);
-
+	if (pipes->fd_in != 0)
+		close(pipes->fd_in);
     // If there's a next command, set up the pipe
-    if (command->next_sibling != NULL)
-    {
-        close(pipes->pipefds[WRITE_END]);  // Close the write end in the parent
-        pipes->fd_in = pipes->pipefds[READ_END];  // Save the read end for the next command's input
-    }
-
-    pipes->last_pid = pipes->pid;  // Store the last process ID
+	if (command->next_sibling != NULL)
+	{
+		close(pipes->pipefds[WRITE_END]);// Close the write end in the parent
+		pipes->fd_in = pipes->pipefds[READ_END];  // Save the read end for the next command's input
+	}
+	pipes->last_pid = pipes->pid;  // Store the last process ID
 }
 
 //returns 0 if no builtin
@@ -87,22 +85,19 @@ int ft_handle_builtin(t_ast_node *ast_tree, t_env **env_list)
     // Check if there is only one command and it is a built-in
     if (command->next_sibling == NULL && command->first_child->next_sibling != NULL && is_builtin(command) == 1)
     {
-        original_stdout = dup(STDOUT_FILENO);
-        if (original_stdout == -1)
-        {
-            perror("dup");
-            return (-1);
-        }
+	original_stdout = dup(STDOUT_FILENO);
+	if (original_stdout == -1)
+		return (perror("dup"), -1);
         // Handle output redirection
-        out = output_redir(command);
-        if (out != -3)
-            handle_dup_and_close(out, STDOUT_FILENO);
+	out = output_redir(command);
+	if (out != -3)
+		handle_dup_and_close(out, STDOUT_FILENO);
         // Execute the built-in command
-        err_code = ft_exec_builtin(command, env_list);
-        set_exit_code(env_list, err_code);
+	err_code = ft_exec_builtin(command, env_list);
+	set_exit_code(env_list, err_code);
         // Restore stdout
-        handle_dup_and_close(original_stdout, STDOUT_FILENO);
-        return (1);
+	handle_dup_and_close(original_stdout, STDOUT_FILENO);
+	return (1);
 	}
 	return (0);
 }
@@ -131,25 +126,22 @@ int	ft_exit_status(pid_t *last_pid)
 		}
 		else if (WTERMSIG(status))
 		{
-            //here should retirn 128+signal with WTERMSIG
 			exit_status = 128 + WTERMSIG(status);
-            //printf("Last command did not exit normally\n");
 			return (exit_status); // Indicate abnormal termination
 		}
 	}
 	return (0); // Return -1 if there was no child process,
 }
 
-int ft_executor(t_ast_node *ast_tree, t_env **env_list)
+int	ft_executor(t_ast_node *ast_tree, t_env **env_list)
 {
-	t_pipe pipes;
-	int			last_exit_status;
+	t_pipe	pipes;
+	int		last_exit_status;
 
 	pipes.fd_in = 0;
 	pipes.last_pid = -1;
 	while (ast_tree->first_child != NULL)
 	{
-        // Create pipe only if there is another command after this one
 		if (ast_tree->first_child->next_sibling != NULL)
 		{
 			if (pipe(pipes.pipefds) == -1)
@@ -166,7 +158,7 @@ int ft_executor(t_ast_node *ast_tree, t_env **env_list)
 	}
 	last_exit_status = ft_exit_status(&pipes.last_pid);
 	set_exit_code(env_list, last_exit_status);
-    // Wait for all other child processes to finish
-	while (wait(NULL) > 0);
+	while (wait(NULL) > 0)
+		;
 	return (0);
 }
